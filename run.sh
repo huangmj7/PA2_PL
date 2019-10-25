e5363179563379e8d27a3f3e9d41a5abff7a0185
huangmj7 <huangm7@rpi.edu>
2019-10-25 16:11:21 -0400

    New run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,8 @@ then
 	echo "Running program..."
 	# Use the following line if you want to disable networking for some reason
 	#erl -pa ./main.erl -run main -run init stop -noshell
-	erl -sname example1@localhost -setcookie thecookiemonster -pa ./main.erl -run main -run init stop -noshell
+	erl -sname example1@localhost -setcookie thecookiemonster -noshell&
+	erl -sname example2@localhost -setcookie thecookiemonster -pa ./main.erl -run main -run init stop -noshell
 else
 	echo "Compilation failed."
 fi
